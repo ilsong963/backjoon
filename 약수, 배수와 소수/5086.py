466d8b82fe6e96e63497c416992b1e8a2d0db0ef
ilsong963 <ilsong963@naver.com>
2024-06-17 20:04:52 +0900

    https://www.acmicpc.net/problem/2501

--- 약수, 배수와 소수/5086.py
+++ 약수, 배수와 소수/5086.py
@@ -1,11 +1,12 @@
-while True:
-    n1,n2=map(int,input().split())
+N,M = map(int, input().split())
 
-    if n1==0 and n2==0:
-        break
-    elif n2%n1==0:
-        print('factor')
-    elif n1%n2==0:
-        print('multiple')
-    else:
-        print('neither')
+list = []
+
+for i in range(1,N+1):
+    if N%i == 0:
+        list.append(i)
+
+if len(list) >= M:
+    print(list[M-1])
+else:
+    print(0)
